0189c403d1faae0a9ac4dc174c75553446de2f61
vibhakantpandey <43365769+vibhakantpandey@users.noreply.github.com>
2018-09-19 15:44:38 +0530

    Initial Version Commit

---
+++ ProcessUserDetail/src/main/java/com/test/mvn/core/App.java
@@ -0,0 +1,19 @@
+package com.test.mvn.core;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        List<String> userList = new ArrayList<String>();
+    	userList.add("Vibha Kant Pandey");
+    	userList.add("Nisha Kant Pandey");
+		userList.add("XYZ ABC");
+        System.out.println( "Hello World!" );
+    }
+}
